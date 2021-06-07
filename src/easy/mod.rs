43e2d066afebb34380f6e6e0b0a7d01a9bfcdaf5
mbuyankin <mbuyankin@gmail.com>
2021-06-07 23:41:09 +0300

    Easy task - 1446. Consecutive Characters

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,3 +1,4 @@
+pub mod task_01446;
 pub mod task_01450;
 pub mod task_01455;
 pub mod task_01460;
---
+++ src/easy/task_01446.rs
@@ -0,0 +1,50 @@
+// 1446. Consecutive Characters
+// https://leetcode.com/problems/consecutive-characters/
+
+use crate::Solution;
+
+impl Solution {
+    pub fn max_power(s: String) -> i32 {
+        let (mut max_res, mut res) = (1, 0);
+        let mut prev_char = ' ';
+
+        for cur_char in s.chars() {
+            match cur_char == prev_char {
+                true => {
+                    res += 1;
+                    max_res = max_res.max(res);
+                }
+                false => {
+                    res = 1;
+                    prev_char = cur_char;
+                }
+            };
+        }
+        max_res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(2, Solution::max_power("leetcode".to_owned()));
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(5, Solution::max_power("abbcccddddeeeeedcba".to_owned()));
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(5, Solution::max_power("triplepillooooow".to_owned()));
+    }
+    
+    #[test]
+    fn test_4() {
+        assert_eq!(11, Solution::max_power("hooraaaaaaaaaaay".to_owned()));
+    }
+}
