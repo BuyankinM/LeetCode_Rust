d19bd916a1df4206f19908ddda27daee01502473
mbuyankin <mbuyankin@gmail.com>
2021-10-16 18:41:31 +0300

    Error in comment

--- src/easy/task_00374.rs
+++ src/easy/task_00374.rs
@@ -3,14 +3,12 @@
 
 use crate::Solution;
 
-/**
-* Forward declaration of guess API.
- * @param  num   your guess
- * @return       -1 if num is lower than the guess number
- *               1 if num is higher than the guess number
- *               otherwise return 0
- * unsafe fn guess(num: i32) -> i32 {}
-*/
+// Forward declaration of guess API.
+// @param  num   your guess
+// @return       -1 if num is lower than the guess number
+//                1 if num is higher than the guess number
+//                otherwise return 0
+// unsafe fn guess(num: i32) -> i32 {}
 
 impl Solution {
     unsafe fn guessNumber(n: i32) -> i32 {
