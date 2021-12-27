e5bd834e2c1e8d52a7d1087aa8e0470a6aa24ac4
mbuyankin <mbuyankin@gmail.com>
2021-12-27 13:39:27 +0300

    Easy. Task 693

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -35,6 +35,7 @@ pub mod task_00496;
 pub mod task_00543;
 pub mod task_00563;
 pub mod task_00575;
+pub mod task_00693;
 pub mod task_00696;
 pub mod task_00697;
 pub mod task_00700;
---
+++ src/easy/task_00693.rs
@@ -0,0 +1,43 @@
+// 693. Binary Number with Alternating Bits
+// https://leetcode.com/problems/binary-number-with-alternating-bits/
+
+use crate::Solution;
+
+impl Solution {
+    pub fn has_alternating_bits(mut n: i32) -> bool {
+        let mut prev_bit = -1;
+        while n > 0 {
+            match n & 1 {
+                bit if bit == prev_bit => return false,
+                bit => prev_bit = bit,
+            }
+            n >>= 1;
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert!(Solution::has_alternating_bits(5));
+    }
+
+    #[test]
+    fn test_2() {
+        assert!(!Solution::has_alternating_bits(7));
+    }
+
+    #[test]
+    fn test_3() {
+        assert!(Solution::has_alternating_bits(1398101));
+    }
+
+    #[test]
+    fn test_4() {
+        assert!(!Solution::has_alternating_bits(447392426));
+    }
+}
