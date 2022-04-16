43457abb7f9a8add8d40623f7e87f6ee3f7d072b
mbuyankin <mbuyankin@gmail.com>
2022-04-16 23:54:49 +0300

    Easy. Task 367

---
+++ src/easy/task_00367.rs
@@ -0,0 +1,75 @@
+// 367. Valid Perfect Square
+// https://leetcode.com/problems/valid-perfect-square/
+
+use crate::Solution;
+
+impl Solution {
+    pub fn is_perfect_square(num: i32) -> bool {
+        use std::cmp::Ordering;
+        let (mut low, mut high) = (0, num);
+        while low <= high {
+            let mid = low + (high - low) / 2;
+            match mid.checked_mul(mid) {
+                Some(square) => match square.cmp(&num) {
+                    Ordering::Equal => return true,
+                    Ordering::Less => low = mid + 1,
+                    Ordering::Greater => high = mid - 1,
+                },
+                None => high = mid - 1,
+            }
+        }
+        false
+    }
+
+    pub fn is_perfect_square_func(num: i32) -> bool {
+        (1..=num)
+            .take_while(|&x| x.checked_mul(x).map_or(false, |res| res <= num))
+            .last()
+            .map_or(false, |x| x * x == num)
+    }
+
+    // https://leetcode.com/problems/valid-perfect-square/discuss/735543/Rust-Solutions
+    pub fn is_perfect_square_1357(num: i32) -> bool {
+        let mut num = num;
+        let mut i = 1;
+        while num > 0 {
+            num -= i;
+            i += 2;
+        }
+        num == 0
+    }
+
+    // https://leetcode.com/problems/valid-perfect-square/discuss/735543/Rust-Solutions
+    pub fn is_perfect_square_newton(num: i32) -> bool {
+        let mut i = num;
+        while i > num / i {
+            i = (i + num / i) / 2;
+        }
+        i * i == num
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert!(Solution::is_perfect_square(1));
+    }
+
+    #[test]
+    fn test_2() {
+        assert!(Solution::is_perfect_square(16));
+    }
+
+    #[test]
+    fn test_3() {
+        assert!(!Solution::is_perfect_square(2147483647));
+    }
+
+    #[test]
+    fn test_4() {
+        assert!(Solution::is_perfect_square(808201));
+    }
+}
