554c2920bd4b102a77e47e2c64b52aaa3dcc342d
mbuyankin <mbuyankin@gmail.com>
2022-05-13 13:36:10 +0300

    Easy. Task 100

---
+++ src/easy/task_00100.rs
@@ -0,0 +1,42 @@
+// 100. Same Tree
+// https://leetcode.com/problems/same-tree/
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use crate::Solution;
+use std::cell::RefCell;
+use std::rc::Rc;
+type OptionNode = Option<Rc<RefCell<TreeNode>>>;
+
+impl Solution {
+    pub fn is_same_tree(p: OptionNode, q: OptionNode) -> bool {
+        match (p, q) {
+            (None, None) => true,
+            (Some(p), Some(q)) => {
+                let p = p.borrow();
+                let q = q.borrow();
+                p.val == q.val
+                    && Solution::is_same_tree(p.left.clone(), q.left.clone())
+                    && Solution::is_same_tree(p.right.clone(), q.right.clone())
+            }
+            _ => false,
+        }
+    }
+}
