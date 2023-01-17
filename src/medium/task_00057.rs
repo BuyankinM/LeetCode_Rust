6441ea0809332f28805569bec743639f950ff829
mbuyankin <mbuyankin@gmail.com>
2023-01-17 09:53:36 +0300

    New short solution

--- src/medium/task_00057.rs
+++ src/medium/task_00057.rs
@@ -63,6 +63,29 @@ impl Solution {
 
         res
     }
+
+    // https://leetcode.com/problems/insert-interval/solutions/395709/beautiful-rust-solution-o-n-0-ms/?orderBy=most_votes&languageTags=rust
+    pub fn insert_short(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+        use std::cmp::{max, min};
+        let mut less = vec![];
+        let mut more = vec![];
+        let mut start = new_interval[0];
+        let mut end = new_interval[1];
+
+        for curr in intervals {
+            if curr[1] < new_interval[0] {
+                less.push(curr);
+            } else if curr[0] > new_interval[1] {
+                more.push(curr);
+            } else {
+                start = min(curr[0], start);
+                end = max(curr[1], end);
+            }
+        }
+        less.push(vec![start, end]);
+        less.append(&mut more);
+        less
+    }
 }
 
 #[cfg(test)]
